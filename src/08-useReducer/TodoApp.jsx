3e63a5b8c39d37fc9daf05b0ec4eb88eb557710b
ddessilvestri <silvestri.des@gmail.com>
2022-08-24 20:41:21 -0300

    onToggleTodo and onDeleteTodo reducer ready

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,24 +1,33 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 import { todoReducer } from "./todoReducer";
 import { TodoList } from "./components/TodoList";
 import { TodoAdd } from "./components/TodoAdd";
 
 const initialState = [
-    {
-        id: new Date().getTime(),
-        description: 'Recolectar la piedra del alma',
-        done:false,
-    },
-    {
-        id: new Date().getTime() * 3,
-        description: 'Recolectar la piedra del poder',
-        done:false,
-    },
+    // {
+    //     id: new Date().getTime(),
+    //     description: 'Recolectar la piedra del alma',
+    //     done:false,
+    // },
+    // {
+    //     id: new Date().getTime() * 3,
+    //     description: 'Recolectar la piedra del poder',
+    //     done:false,
+    // },
 ];
+
+const init = () =>{
+    return JSON.parse( localStorage.getItem('todos'))  || [];
+};
 
 export const TodoApp = () => {
 
-    const [todos, dispatch] = useReducer(todoReducer, initialState);
+    const [todos, dispatch] = useReducer(todoReducer, initialState,init);
+
+    useEffect(() => {
+        localStorage.setItem('todos',JSON.stringify(todos));
+    }, [todos])
+    
 
     const handleNewTodo = (todo) =>{
         const action = {
@@ -28,6 +37,26 @@ export const TodoApp = () => {
 
         dispatch(action);
     }
+
+    const handleDeleteTodo = (id) =>{
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id,
+        })
+    };
+
+    const handleToggleTodo = (id)=>{
+        console.log("handleToggleTodo",id);
+        // dispatch({
+        //     type: '[TODO] Toggle Todo',
+        //     payload: id,
+        // })
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id,
+        });
+    }
+
     
     return (
         <>
@@ -36,7 +65,11 @@ export const TodoApp = () => {
             
             <div className="row">
                 <div className="col-7">
-                    <TodoList todos = {todos} />
+                    <TodoList 
+                        todos = {todos} 
+                        onDeleteTodo = {handleDeleteTodo}
+                        onToggleTodo = {handleToggleTodo}
+                        />
                 </div>
                 <div className="col-5">
                 
