1b957975628e8a54b8aa11c92f24d03cda5d1423
ddessilvestri <silvestri.des@gmail.com>
2022-08-23 21:15:19 -0300

    todoApp first test

--- src/main.jsx
+++ src/main.jsx
@@ -11,12 +11,13 @@ import { CallbackHook } from './06-memos/CallbackHook'
 import { MemoHook } from './06-memos/MemoHook'
 import { Memorize } from './06-memos/Memorize'
 import { Padre } from './07-tarea-memo/Padre'
+import { TodoApp } from './08-useReducer/TodoApp'
 import { HooksApp } from './HooksApp'
-import './08-useReducer/intro-reducer'
+//import './08-useReducer/intro-reducer'
 import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   // <React.StrictMode>
-//     <Padre />
-//   // </React.StrictMode>
-// )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+    <TodoApp />
+  // </React.StrictMode>
+)
