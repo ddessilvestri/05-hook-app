6179f46246735a26063dd2ba4afc9fee6f07e746
ddessilvestri <silvestri.des@gmail.com>
2022-08-25 19:57:45 -0300

    BrowserRouter + Router V6

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { BrowserRouter } from "react-router-dom";
 import { CounterApp } from './01-useState/CounterApp'
 import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import { FromWithCustomHook } from './02-useEffect/FormWithCustomHook'
@@ -12,12 +14,15 @@ import { MemoHook } from './06-memos/MemoHook'
 import { Memorize } from './06-memos/Memorize'
 import { Padre } from './07-tarea-memo/Padre'
 import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
 import { HooksApp } from './HooksApp'
 //import './08-useReducer/intro-reducer'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <TodoApp />
-  // </React.StrictMode>
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <MainApp />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
 )
