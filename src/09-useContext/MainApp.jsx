12544f08cebc1ce2fd92b88e8d5bda32be3ceeea
ddessilvestri <silvestri.des@gmail.com>
2022-08-30 03:04:50 -0300

    NavBar App

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,10 +1,25 @@
+import { Routes,Route,Navigate } from "react-router-dom"
+import { AboutPage } from "./AboutPage";
+import { HomePage } from "./HomePage";
+import { LoginPage } from "./LoginPage";
+import { NavBar } from "./NavBar";
 
     
     export const MainApp = () => {
         return (
-            <>
-                <h1>MainApp</h1>
+            <>                
+                <NavBar />
                 <hr/>
+                
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="about" element={<AboutPage />} />
+                    <Route path="login" element={<LoginPage />} />
+
+                    <Route path="*" element={<Navigate to="/about"/>}/>
+                    {/* <Route path="*" element ={<LoginPage />} /> */}
+
+                </Routes>
             </>
         )
     }
